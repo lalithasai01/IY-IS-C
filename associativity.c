a8985f5b56e4e0f3399c28410dafd3d652a33b65
TELU LALITHA SAI <chittilaharit@gmail.com>
2025-11-26 16:50:43 +0530

    Create associativity.c for adding five floats
    Implement a program to add five float values.

---
+++ associativity.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a,b,c,d,e,f;
+    printf("Enter the values");
+    scanf("%f%f%f%f%f",&a,&b,&c,&d,&e);
+    f=a+b+c+d+e;
+    printf("Addition of the values:%.f",f);
+    return 0;
+
+}
